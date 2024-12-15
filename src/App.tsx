1893d623352eeaaee80f4206c92557a6188213d3
k0va9 <179985748+k0va9@users.noreply.github.com>
2024-12-15 20:04:40 +0900

    feat: add create task field

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,15 @@ import type { TaskType } from './components/atoms/Task'
 function App() {
   const [tasklist, setTask] = useState<Array<TaskType>>([])
 
+  const createTask = (val: string) => {
+    const data: TaskType = {
+      id: Math.random().toString(36).slice(-8),
+      name: val,
+      completed: false
+    }
+    setTask([...tasklist, data])
+  }
+
   return (
     <section className="max-w-[70vw] mx-auto mt-16 h-[100vh] container">
       {!tasklist.length ? (
@@ -14,6 +23,17 @@ function App() {
           return <Task {...item} />
         })
       )}
+      <div className="max-w-[40vw] mx-auto my-8">
+        <input
+          type="text"
+          className="border p-2 w-full"
+          placeholder="タスクを作成"
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') createTask(e.currentTarget.value)
+            e.currentTarget.value = ''
+          }}
+        />
+      </div>
     </section>
   )
 }
