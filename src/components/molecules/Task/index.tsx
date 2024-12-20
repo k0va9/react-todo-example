8a4ba81160538fa8be7a1b3771675347afe9940d
k0va9 <179985748+k0va9@users.noreply.github.com>
2024-12-20 20:22:12 +0900

    feat: manage input value using useState

--- src/components/molecules/Task/index.tsx
+++ src/components/molecules/Task/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Input } from '../../atoms/Input'
 import { Label } from '../../atoms/Label'
 
@@ -15,14 +16,14 @@ type handlerType = {
 type Props = TaskType & handlerType
 
 export const Task = ({ ...props }: Props) => {
+  const [task,setTask] = useState("");
   return (
     <div className="w-full flex gap-4 text-xl border-b pb-4">
       <Input
         id={props.id}
         type="checkbox"
-        onChange={(e) => {
-          props.handleChanged(e.target.id)
-        }}
+        value={task}
+        onChange={(e)=>{setTask(e.target.value)}}
       />
       <Label className={props.completed ? 'line-through' : ''}>
         {props.name}
